f635cfa87807b065c25d4f6875d6445b7ca43e52
Ajith Jose <79376645+ajithjos@users.noreply.github.com>
2025-01-25 17:34:43 +0000

    Bringup : 1

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages, find_packages
+
 
 setup(
     name="dhenara",
     version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "sdk"},
+    package_dir={"": "src"},  # Tell setuptools packages are under src
+    # packages=find_namespace_packages(where="src"),  # Find all packages under src
+    packages=find_packages(where="src"),  # Finds all packages under src
     install_requires=[
         "httpx>=0.24.0",
         "requests>=2.25.1",
