2b4217f0c9458a2b098eb3c7ffd499b6f7f81132
Ajith Jose <79376645+ajithjos@users.noreply.github.com>
2025-05-11 11:28:42 +0100

    Package Release Updages - 3:

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
+import os
+import re
+
 from setuptools import find_namespace_packages, setup
+
+version = None
+# Read version without importing the package
+with open(os.path.join("src/dhenara/agent", "__init__.py")) as f:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string")
+
 
 setup(
     name="dhenara-agent",
-    version="0.1.0",
+    version=version,
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src", include=["dhenara.*"]),
     install_requires=[
+        "dhenara-ai>=1.0.0",
         "click>=8.0.0",  # CLI
         "pyyaml>=6.0",  # CLI
         "httpx>=0.24.0",
         "requests>=2.25.1",
         "pydantic>=2.0.0",
-        "dhenara>=1.0.0",  # Dependency on dhenai
-        # TODO_FUTURE: Add a config without observability dependecy
+        # Observability dependencies: # TODO_FUTURE: Add a config
         "opentelemetry-api>=1.20.0",
         "opentelemetry-sdk>=1.20.0",
         "opentelemetry-instrumentation>=0.40b0",
@@ -21,7 +34,7 @@ setup(
     ],
     extras_require={
         "observability": [
-            # Tracing Visualization
+            # Additional tracing visualization
             "opentelemetry-exporter-jaeger>=1.20.0",
         ],
         "dev": [
@@ -32,16 +45,28 @@ setup(
         ],
     },
     python_requires=">=3.10",
-    description="Dhenara Inc AI-Agent Platform SDK",
+    description="Dhenara Agent DSL (DAD) Framework SDK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Dhenara",
     author_email="support@dhenara.com",
-    url="https://github.com/dhenara/dhen-agent",
+    url="https://github.com/dhenara/dhenara-agent",
+    license="MIT",
+    keywords="ai, llm, machine learning, language models, ai agents, agent frameworks",
+    project_urls={
+        "Homepage": "https://dhenara.com",
+        "Documentation": "https://docs.dhenara.com/",
+        "Bug Reports": "https://github.com/dhenara/dhenara-agent/issues",
+        "Source Code": "https://github.com/dhenara/dhenara-agent",
+    },
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
     ],
     # CLI
     entry_points={
